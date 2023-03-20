c392bbd8dd36599bb5bf719ba3eb5ea2c864b884
Leonardo Araújo Freitas <62023102+Leonardo1924@users.noreply.github.com>
2023-03-20 17:59:44 +0000

    Add files via upload

--- Fichas_Práticas/Ficha3/ficha3.c
+++ Fichas_Práticas/Ficha3/ficha3.c
@@ -27,6 +27,73 @@ int ex1b(){
     return 0;
 }
 
+void swapM(int *x, int * y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void swap(int v[], int i, int j){
+    int *pV = v;
+    int *pVi = v + i;
+    int *pVj = v + j;
+    swapM(pVi,pVj); 
+}
+
+int soma (int v[], int N){
+    int *pV = v;
+    int soma = 0;
+    for(int  i = 0 ; i < N; i++){
+        soma += *(pV + i);
+    }
+    return soma;
+}
+
+void inverteArray(int v[], int N){
+    int *pV = v;
+    int *pVf = v + N - 1;
+    for(int i = 0; i < N/2; i++){
+        swapM(pV + i, pVf - i);
+    }
+}
+
+int maximum(int v[],int N, int *m){
+    *m = 0;
+    int* pV = v;
+    int value = 0;
+    for(int i = 0 ; i < N ; i++){
+        value = *(pV + i);
+        if (value > *m )
+            *m = value;
+    }
+    return 0;
+}
+
+void quadrados(int q[], int N){
+    int* pQ = q;
+    for(int i = 0 ; i < N ; i++){
+        *(pQ + i) = pow(q[i],2);
+    }
+}
+
+void pascal(int v[], int N){
+    int* pV = v;
+    if(N == 1) *pV = 1;
+    else{
+        int linhaAnterior[N-1];
+        pascal(linhaAnterior, N-1);
+        *pV = 1;
+        *(pV + N - 1) = 1;
+        for(int i = 1; i < N -1;i++){
+            *(pV + i) = *(linhaAnterior + i - 1) + *(linhaAnterior + i);
+        }
+    } 
+}
+
+
+
+
+// MAIN //
 
 int main(int argc, char *argv[]){
     char str1[] = "ex1a";
@@ -42,5 +109,78 @@ int main(int argc, char *argv[]){
     if (strcmp(argv[1], str2) == 0){
         ex1b();
         // 13
+    }
+    char str3[] = "ex2";
+    if (strcmp(argv[1], str3) == 0){
+        int x = atoi(argv[2]);
+        int y = atoi(argv[3]);
+        swapM(&x, &y);
+        printf("%d %d\n", x, y);
+    }
+    char str4[] = "ex3";
+    if(strcmp(argv[1], str4) == 0){
+        int v[10] = {1,2,3,4,5,6,7,8,9,10};
+        int indice1 = atoi(argv[2]);
+        int indice2 = atoi(argv[3]);
+        swap(v, indice1 , indice2);
+        for(int i = 0; i < 10; i++){
+            printf("%d ", v[i]);
+        }
+        printf("\n");
+    }
+    char str5[] = "ex4";
+    if(strcmp(argv[1], str5) == 0){
+        int v[10] = {2,2,2,2,2,2,2,2,2,2};
+        printf("%d\n", soma(v, 10));
+    }
+    char str6[] = "ex5";
+    if(strcmp(argv[1], str6) == 0){
+        int v[11] = {1,2,3,4,5,6,7,8,9,10,11};
+        inverteArray(v, 11);
+        for(int i = 0; i < 11; i++){
+            printf("%d ", v[i]);
+        }
+        printf("\n");
+    }
+    char str7[] = "ex6";
+    if(strcmp(argv[1], str7) == 0){
+        int v[10] = {10,20,30,40,45,64,74,84,130,101};
+        int m;
+        maximum(v, 10, &m);
+        printf("Máximo: %d\n", m);
+    }
+    char str8[] = "ex7";
+    if(strcmp(argv[1], str8) == 0){
+        int v[10] = {1,2,3,4,5,6,7,8,9,10};
+        quadrados(v, 10);
+        for(int i = 0; i < 10; i++){
+            printf("%d ", v[i]);
+        }
+        printf("\n");
+    }
+    char str9[] = "pascal";
+    if(strcmp(argv[1], str9) == 0){
+        int v[100];
+        int N = atoi(argv[2]);
+        pascal(v, N);
+        for(int i = 0; i < N; i++){
+                printf("%d ", v[i]);
+            }
+            printf("\n");
+    }
+    char str10[] = "fullpascal";
+    if(strcmp(argv[1], str10) == 0){
+        int N = atoi(argv[2]);
+        for(int i = 1; i <= N; i++){
+            int v[i];
+            pascal(v, i);
+            for(int j = 0; j < N - i; j++){
+                printf(" ");
+            }
+            for(int j = 0; j < i; j++){
+                printf("%d ", v[j]);
+            }
+            printf("\n");
+        }
     }
 }
