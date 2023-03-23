88d54466d68437996494a52cdc50625c9d8b67e9
Leonardo Araújo Freitas <62023102+Leonardo1924@users.noreply.github.com>
2023-03-23 15:54:28 +0000

    Add files via upload

--- Memória automática/auto.c
+++ Memória automática/auto.c
@@ -2,20 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-int max(int x, int y){
-    if(x > y)
+int max(int x, int y)
+{
+    if (x > y)
         return x;
     else
         return y;
 }
 
-int one(){
+int one()
+{
     int max = 0;
     while (1)
     {
         int num;
         scanf("%d", &num);
-        if(num == 0)
+        if (num == 0)
             break;
         if (num > max)
             max = num;
@@ -23,29 +25,34 @@ int one(){
     return max;
 }
 
-int two(){
+int two()
+{
     int mean = 0;
     int count = 0;
-    while (1){
+    while (1)
+    {
         int num;
         scanf("%d", &num);
-        if(num == 0)
+        if (num == 0)
             break;
         mean += num;
         count++;
     }
-    return mean/count;
+    return mean / count;
 }
 
-int three(){
+int three()
+{
     int max = 0;
     int max2 = 0;
-    while (1){
+    while (1)
+    {
         int num;
         scanf("%d", &num);
-        if(num == 0)
+        if (num == 0)
             break;
-        if (num > max){
+        if (num > max)
+        {
             max2 = max;
             max = num;
         }
@@ -53,9 +60,11 @@ int three(){
     return max2;
 }
 
-int bitsUm(unsigned int n){
+int four(unsigned int n)
+{
     int count = 0;
-    while (n != 0){
+    while (n != 0)
+    {
         if (n % 2 == 1)
             count++;
         n = n / 2;
@@ -63,31 +72,38 @@ int bitsUm(unsigned int n){
     return count;
 }
 
-int trailingZ(unsigned int n){
+int five(unsigned int n)
+{
     int count = 0;
-    while (n % 2 == 0){
+    while (n % 2 == 0)
+    {
         count++;
         n = n / 2;
     }
     return count;
 }
 
-int qDig(unsigned int n){
+int six(unsigned int n)
+{
     int count = 0;
-    while (n != 0){
+    while (n != 0)
+    {
         count++;
         n = n / 10;
     }
     return count;
 }
 
-char *strcatByLeo(char s1[], char s2[]){
+char *seven(char s1[], char s2[])
+{
     int i = 0;
     int j = 0;
-    while (s1[i] != '\0'){
+    while (s1[i] != '\0')
+    {
         i++;
     }
-    while (s2[j] != '\0'){
+    while (s2[j] != '\0')
+    {
         s1[i] = s2[j];
         i++;
         j++;
@@ -96,12 +112,78 @@ char *strcatByLeo(char s1[], char s2[]){
     return s1;
 }
 
+char *eight(char *dest, char source[])
+{
+    int i = 0;
+    while (source[i] != '\0')
+    {
+        dest[i] = source[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
 
+int nine(char s1[], char s2[])
+{
+    int i = 0;
+    while (s1[i] != '\0' && s2[i] != '\0')
+    {
+        if (s1[i] > s2[i])
+            return 1;
+        else if (s1[i] < s2[i])
+            return -1;
+        i++;
+    }
+    if (s1[i] == '\0' && s2[i] == '\0')
+        return 0;
+    else if (s1[i] == '\0')
+        return -1;
+    else
+        return 1;
+}
 
+char *ten(char s1[], char s2[])
+{
+    char *ret = NULL;
+    int i, j, l;
+    if (!(*s2))
+        ret = s1;
+    for (i = 0; s1[i] && !ret; i++)
+    {
+        if (s1[i] == *s2)
+        {
+            for (j = i, l = 0; s2[l] && s1[j] == s2[l]; j++, l++)
+                ;
+            if (!s2[l])
+                ret = s1 + i;
+        }
+    }
+    return ret;
+}
 
-int main(){
-    char x[] = "Hello";
-    char y[] = "World";
-    printf("%s\n", strcatByLeo(x, y));
+void eleven(char s[])
+{
+    int i, j, k = 0, count = 0;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        count++;
+    }
+
+    char s2[count];
+    for (int j = count-1; j >= 0; j--)
+    {
+        s2[k] = s[j];
+        k++;
+    }
+    s2[k] = '\0';
+}
+
+int main()
+{
+    char x[6] = "MACACO";
+    char y[6] = "";
+    eleven(x);
+
     return 0;
 }
