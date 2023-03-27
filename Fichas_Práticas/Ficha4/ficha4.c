6571b0a4dc6998d631c951b8da3b9cae4dffbab6
Leonardo Araújo Freitas <62023102+Leonardo1924@users.noreply.github.com>
2023-03-27 11:32:47 +0100

    Add files via upload

--- Fichas_Práticas/Ficha4/ficha4.c
+++ Fichas_Práticas/Ficha4/ficha4.c
@@ -99,6 +99,20 @@ void merge (int a[], int na, int b[], int nb, int r[]){
     }
 }
 
+int partition(int v[], int N, int x){
+    int i = 0, j = N-1;
+    while(i < j){
+        while(v[i] < x) i++;
+        while(v[j] >= x) j--;
+        if(i < j){
+            int temp = v[i];
+            v[i] = v[j];
+            v[j] = temp;
+        }
+    }
+    return i;
+} 
+
 int main(int argc, char *argv[]){
     char str1[] = "contaVogais";
     if(strcmp(argv[1], str1) == 0){
