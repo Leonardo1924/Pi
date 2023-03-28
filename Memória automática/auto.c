0f464174cd8b8c192ffc60bdd8ea3bb6ec66cf38
Leonardo1924 <leonardo1924@protonmail.com>
2023-03-28 22:04:05 +0100

    First 50

--- Memória automática/auto.c
+++ Memória automática/auto.c
@@ -11,6 +11,11 @@ int max(int x, int y)
         return y;
 }
 
+int min(int a,int b){
+    if (a < b) return a;
+    else return b;
+}
+
 void swapW(char *x, char *y)
 {
     int temp = *x;
@@ -346,8 +351,342 @@ int palindrome (char s[]) {
     return i == N;
 }
 
+void tail(char* s){
+    int i;
+    for(i=0; s[i]; i++) s[i] = s[i+1];
+}
 
+int remRep(char x[]){
+    int i;
+    for(i = 0; x[i]; i++)
+        while(x[i+1] == x[i]) tail(x+i+1);
+    return i;
+}
 
+int limpaEspacos(char t[]){
+    int i;
+    for(i=0; t[i];i++)
+        while(t[i] == ' ' && t[i+1] == t[i]) tail(t+i+1);
+    return i;
+}
+
+void insere(int v[], int N, int x){
+    int i,j, inserted = 0;
+    for(i= 0; i < N && !inserted; i++){
+        if (x < v[i]){
+            for(j = N ; j > i; j--){
+                v[j] = v[j-1];
+            }
+            v[i] = x;
+            inserted = 1;
+        }
+    }
+    if(!inserted) v[N] = x;
+}
+
+void merge(int r[], int a[], int b[], int na, int nb){
+    int i = 0, j = 0;
+    while( i + j < na + nb ){
+        if(j== nb || a[i] < b[j]){
+            r[i+1] = a[i];
+            i++;
+        }
+        else{
+            r[i+j] = b[j];
+            j++;
+        }
+    }
+}
+
+int crescente (int a[], int i, int j){
+    int k , cresc = 1;
+    for(k = i; k < j && cresc; k++)
+        if(a[k+1] < a[k]) cresc = 0;
+    return cresc;
+}
+
+int retiraNeg(int v[], int N){
+    int i,j;
+    for(i = 0, j= 0; i < N ; i++){
+        if(v[i] >= 0){
+            v[j] = v[i];
+            j++;
+        }
+    }
+    return j;
+}
+
+int menosFreq(int v[], int N){
+    int i, freq = 1, minFreq = N, ans = v[0];
+    for(i = 1; i < N; i++){
+        if(v[i] == v[i-1]) freq++;
+        else{
+            if(freq < minFreq){
+                minFreq = freq;
+                ans = v[i-1];
+            }
+            freq = 1;
+        }
+    }
+    if(freq < minFreq){
+        minFreq = freq;
+        ans = v[i-1];
+    }
+    
+    return ans;
+}
+
+int maisFreq (int v[], int N) {
+    int i, freq = 1, maxFreq = 0, ans = v[0];
+    for(i = 1; i < N; i++) {
+        if(v[i] == v[i - 1]) freq++;
+        else {
+            if(freq > maxFreq) {
+                maxFreq = freq;
+                ans = v[i - 1];
+            }
+            freq = 1;
+        }
+    }
+    if(freq > maxFreq) {
+        maxFreq = freq;
+        ans = v[i - 1];
+    }
+    return ans;
+}
+
+int maxCresc(int v[], int N){
+    int i, ans = 1, cres = 1;
+    for(i = 1; i < N; i++){
+        if(v[i] > v[i-1]) cres++;
+        else{
+            ans = max(ans, cres);
+            cres = 1;
+        }
+    }
+    return max(ans, cres);
+}
+
+int elimRep(int v[], int n) {
+    int i, j, k;
+
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n;) {
+            if (v[j] == v[i]) {
+                for (k = j; k < n - 1; k++) {
+                    v[k] = v[k + 1];
+                }
+                n--;
+            } else {
+                j++;
+            }
+        }
+    }
+    return n;
+}
+
+int elimRepOrd (int v[], int n){
+    int i, j;
+
+    if(n== 0 || n == 1) return n;
+
+    for(i = 0, j = 1; j < n; j++){
+        if(v[j] != v[i]){
+            i++;
+            v[i] = v[j];
+        }
+    }
+    return i+1;
+}
+
+int comunsOrd (int a[], int na, int b[], int nb){
+    int i, j, count = 0;
+    for(i = 0, j = 0; i < na && j < nb;){
+        if(a[i] == b[j]){
+            count++;
+            i++;
+            j++;
+        }
+        else if(a[i] < b[j]) i++;
+        else j++;
+    }
+    return count;
+}
+
+int comuns (int a[], int na, int b[], int nb){
+    int i, j, count = 0;
+    for(i = 0; i < na; i++){
+        for(j = 0; j < nb; j++){
+            if(a[i] == b[j]){
+                count++;
+                break;
+            }
+        }
+    }
+    return count;
+}
+
+int minInd (int v[], int n){
+    int i, min = v[0], minInd = 0;
+    for(i = 1; i < n; i++){
+        if(v[i] < min){
+            min = v[i];
+            minInd = i;
+        }
+    }
+    return minInd;
+}
+
+void somasAC(int v[], int Ac[], int N){
+    int i , acc = 0 ;
+    for(i = 0; i < N ; i++){
+        acc += v[i];
+        Ac[i] = acc;
+    }
+}
+
+int triSup (int N, int m [N][N]) {
+    int i,j, istrip = 1;
+    for(i = 0; i < N && istrip; i++){
+        for(j = 0 ; j < i && istrip; j++){
+            if(m[i][j] != 0)
+                istrip = 0;
+        }
+    }
+    return istrip;
+}
+
+void transposta (int N, float m[N][N]) {
+    int i, j;
+    float temp;
+    for(i = 0; i < N;i++){
+        for(j = 0; j < i; j++){
+            temp = m[i][j];
+            m[i][j] = m[j][i];
+            m[j][i] = temp;
+        }
+    }
+}
+
+void addTo (int N, int M, int a [N][M], int b [N][M]){
+    int i,j;
+    for(i = 0; i < N;i++){
+        for(j = 0; j < M; j++)
+            a[i][j] += b[i][j];
+    }
+}
+
+int unionSet (int N, int v1[N], int v2[N], int r[N]){
+    int i, len = 0;
+    for(i = 0; i < N; i++){
+        r[i] = v1[i] || v2[i];
+        len += r[i];
+    }
+    return len;
+}
+
+int interesectSet (int N, int v1[N], int v2[N], int r[N]){
+    int i, len = 0;
+    for(i = 0; i < N; i++){
+        r[i] = v1[i] && v2[i];
+        len += r[i];
+    }
+    return len;
+}
+
+int intersectMSet (int N, int v1[N] , int v2[N] , int r[N]){
+    int i, len = 0;
+    for(i = 0; i < N ; i++){
+        r[i] = min(v1[i],v2[i]);
+        len += r[i];
+    }
+    return len;
+}
+
+int unionMSet (int N, int v1[N], int v2[N], int r[N]){
+    int i, len = 0;
+    for(i = 0; i < N ; i++){
+        r[i] = max(v1[i],v2[i]);
+        len += r[i];
+    }
+    return len;
+}
+
+int cardinalMSet (int N, int v[N]){
+    int i , len = 0;
+    for(i = 0; i < N ; i++) len += v[i];
+    return len;
+}
+
+typedef enum movimento {Norte, Oeste, Sul, Este} Movimento;
+typedef struct posicao {
+int x, y;
+} Posicao;
+
+Posicao posFinal (Posicao inicial, Movimento mov[], int N){
+    int i;
+    for(i = 0; i < N; i++){
+        switch(mov[i]){
+            case Norte:
+                inicial.y++;
+                break;
+            case Oeste:
+                inicial.x--;
+                break;
+            case Sul:
+                inicial.y--;
+                break;
+            case Este:
+                inicial.x++;
+                break;
+        }
+    }
+    return inicial;
+}
+
+int caminho (Posicao inicial, Posicao final, Movimento mov[], int N){
+    int i;
+    for(i = 0; i < N; i++){
+        if(inicial.x < final.x){
+            mov[i] = Este;
+            inicial.x++;
+        }
+        else if(inicial.x > final.x){
+            mov[i] = Oeste;
+            inicial.x--;
+        }
+        else if(inicial.y < final.y){
+            mov[i] = Norte;
+            inicial.y++;
+        }
+        else if(inicial.y > final.y){
+            mov[i] = Sul;
+            inicial.y--;
+        }
+        else break;
+    }
+    if(inicial.x == final.x && inicial.y == final.y) return i;
+    else return -1;
+}
+
+int maisCentral (Posicao pos[], int N){
+    int i, min = 1000000, minInd = 0;
+    for(i = 0; i < N; i++){
+        if(pos[i].x*pos[i].x + pos[i].y*pos[i].y < min){
+            min = pos[i].x*pos[i].x + pos[i].y*pos[i].y;
+            minInd = i;
+        }
+    }
+    return minInd;
+}
+
+int vizinhos (Posicao p, Posicao pos[], int N){
+    int i, count = 0;
+    for(i = 0; i < N; i++){
+        if(abs(pos[i].x - p.x) + abs(pos[i].y - p.y) == 1) count++;
+    }
+    return count;
+}
 
 int main()
 {
